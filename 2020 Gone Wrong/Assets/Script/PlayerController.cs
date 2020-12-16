f01845c79a719576da096b1b48788a1d317ca636
Hemkesh10 <b00130251@mytudublin.ie>
2020-12-16 14:11:50 +0000

    Update

--- 2020 Gone Wrong/Assets/Script/PlayerController.cs
+++ 2020 Gone Wrong/Assets/Script/PlayerController.cs
@@ -1,18 +1,40 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+
+    private Rigidbody playerRB;
+    public float jumpForce = 10;
+    public float gravityModifier;
+    public bool isOnGround = true;
+    
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerRB = GetComponent<Rigidbody>();
+        Physics.gravity *= gravityModifier;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+        transform.Translate(Vector3.forward * Time.deltaTime * 5);
+
+        if (Input.GetKeyDown(KeyCode.Space)&& isOnGround)
+        {
+            playerRB.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            isOnGround = false;
+
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        isOnGround = true;
     }
 }
