72db9c48de25d175f7ff557f64dcfe3f676d1faa
Hemkesh10 <b00130251@mytudublin.ie>
2020-12-16 14:54:22 +0000

    Update

--- 2020 Gone Wrong/Assets/Script/MoveLeft.cs
+++ 2020 Gone Wrong/Assets/Script/MoveLeft.cs
@@ -2,13 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FollowPlayer : MonoBehaviour
+public class NewBehaviourScript : MonoBehaviour
 {
-
-    public GameObject player;
-    private Vector3 offset = new Vector3(0, 4, 6);
-    
-    
     // Start is called before the first frame update
     void Start()
     {
@@ -18,6 +13,6 @@ public class FollowPlayer : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.position = player.transform.position + offset;
+        
     }
 }
